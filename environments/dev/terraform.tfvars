b082eb58e1f9490c8c253f5242460072c3499f7f
kventrad <kventrad@gmail.com>
2022-09-05 14:17:48 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="elite-replica-343907"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "elite-replica-343907-tfstate"
     prefix = "env/prod"
   }
 }
